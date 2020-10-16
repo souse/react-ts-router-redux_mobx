c0cafc91f4796654b43f0b903a2b3d27a771f283
ping_zhang <zhangpingcpx@gmail.com>
2020-10-17 00:19:05 +0800

    how

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
 
-import route from '@/routes';
-import * as serviceWorker from './serviceWorker';
-import 'amfe-flexible';
+import * as serviceWorker from './serviceWorker'
+import 'amfe-flexible'
 
-import './index.scss';
+import store, { history } from './redux/store';
+import route from './routes';
+import './index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter children={route} />
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <BrowserRouter children={route} />
+      </ConnectedRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
-import { createStore, combineReducers, compose } from 'redux'
-import { connectRouter } from 'connected-react-router'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
 import { History, createBrowserHistory } from 'history'
 
 import { opCount } from './rcount'
@@ -7,19 +7,24 @@ import { opCount } from './rcount'
 declare var window: Window & { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any }
 
 export const history = createBrowserHistory();
+
+const middlewares = [routerMiddleware(history)];
 const composeEnhancers = (
   process.env.NODE_ENV === 'development' &&
   window &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 ) || compose
-
-const routerReducer = (history: History<any>) => {
+const rootReducer = (history: History<any>) => {
   return combineReducers({
     router: connectRouter(history),
     counter: opCount
   })
 }
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+// create store
+const store = createStore(rootReducer(history), {}, enhancer);
 
+export default store;
 
 
 
